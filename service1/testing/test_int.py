fff989dd164c0fe6c40e79d80a4e1b41248443cd
John Dela Cruz <kenneth1521412@gmail.com>
2020-08-27 13:23:12 +0000

    application fully working

--- service1/testing/test_int.py
+++ service1/testing/test_int.py
@@ -7,7 +7,7 @@ from os import getenv
 from flask_testing import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from service1.app import app
+from service1.app import app, db
 
 
 class TestBase(LiveServerTestCase):
@@ -24,7 +24,7 @@ class TestBase(LiveServerTestCase):
         chrome_options.binary_location = "/usr/bin/chromium-browser"
         chrome_options.add_argument("--headless")
         self.driver = webdriver.Chrome(executable_path="/home/kenneth1521412/SFIA2/chromedriver", chrome_options=chrome_options)
-        self.driver.get("http://34.105.244.35:5000/")
+        self.driver.get("http://34.89.28.103:5000/")
         
         db.session.commit()
         
@@ -34,8 +34,21 @@ class TestBase(LiveServerTestCase):
         print("--------------------------END-OF-TEST----------------------------------------------\n\n\n-------------------------UNIT-AND-SELENIUM-TESTS----------------------------------------------")
 
     def test_server_is_up_and_running(self):
-        response = urlopen("http://34.105.244.35:5000")
+        response = urlopen("http://34.89.28.103:5000")
         self.assertEqual(response.code, 200)
+
+
+class TestView(TestBase):
+    def test_coordinate(self):
+        self.driver.find_element_by_xpath('/html/body/form[1]/button').click()
+        time.sleep(1)
+        assert url_for('home') in self.driver.current_url
+
+    def test_delete(self):
+        self.driver.find_element_by_xpath('/html/body/form[2]/button').click()
+        time.sleep(1)
+        assert url_for('home') in self.driver.current_url
+
 
 
 
