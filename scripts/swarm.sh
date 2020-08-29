f8b8e7c8ca398ca0e5d5421485e70584e29eeff9
John Dela Cruz <kenneth1521412@gmail.com>
2020-08-29 17:47:01 +0000

    swarm jenkins

--- scripts/swarm.sh
+++ scripts/swarm.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 ssh kenneth1521412@manager<< EOF
+docker rmi $(docker images -a -q)
+
 pwd
 whoami
 ls
@@ -10,5 +12,17 @@ ls
 
 git clone https://github.com/mobamba1/SFIA2.git
 ls
+
+cd SFIA2
+
+
+docker login -u"$USER" -p"$PASS"
+docker pull kenneth1521412/service1
+docker pull kenneth1521412/service2
+docker pull kenneth1521412/service3
+docker pull kenneth1521412/service4
+
+docker stack deploy --compose-file docker-compose.yaml SFIA2
+docker stack services SFIA2
 
 EOF
