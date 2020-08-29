df8688ddf48ad8c1e55dd1a5d62e69d429ffd029
John Dela Cruz <kenneth1521412@gmail.com>
2020-08-29 11:57:30 +0000

    jenkins4

--- scripts/build.sh
+++ scripts/build.sh
@@ -22,4 +22,11 @@ docker ps
 ansible-playbook -i inventory.cfg playbook.yaml
 
 docker-compose build
+
+sudo docker-compose build
+sudo docker images
+sudo docker system prune -f
+
+sudo docker images
+sudo docker login
 sudo docker-compose push
