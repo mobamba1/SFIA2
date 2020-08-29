05b12fa6bdc9604a0faa3480640444a89050933e
John Dela Cruz <kenneth1521412@gmail.com>
2020-08-29 17:53:44 +0000

    swarm jenkins

--- Jenkinsfile
+++ Jenkinsfile
@@ -8,7 +8,7 @@ pipeline {
     stages {
         stage("Build") {
             steps {
-                sh "bash ./scripts/swarm.sh"
+                sh "bash ./scripts/build.sh"
                 
             }
         }
--- scripts/test.sh
+++ scripts/test.sh
@@ -10,3 +10,15 @@ pip3 install -r requirements.txt
 cd service1
 python3 -m pytest --cov service1
 cd ..
+
+cd service2
+python3 -m pytest --cov service2
+cd ..
+
+cd service3
+python3 -m pytest --cov service3
+cd ..
+
+cd service4
+python3 -m pytest --cov service4
+cd ..
