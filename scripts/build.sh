bbcf61d7c69e520ca56f1e3c70121b061944d50d
John Dela Cruz <kenneth1521412@gmail.com>
2020-08-29 13:18:23 +0000

    jenkins5

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,28 +5,28 @@ ls
 
 #docker rmi $(docker images -a -q)
 
-deactivate 
-docker-compose down
+#deactivate 
+#docker-compose down
 
-docker --version
-docker-compose --version
+#docker --version
+#docker-compose --version
 
-ansible --version
+#ansible --version
 
 
 
-docker images
-docker ps
+#docker images
+#docker ps
 
-ansible-playbook -i inventory.cfg playbook.yaml
+#ansible-playbook -i inventory.cfg playbook.yaml
 
-docker-compose build
+#docker-compose build
 
-sudo docker-compose build
-sudo docker images
-sudo docker system prune -f
+#sudo docker-compose build
+#sudo docker images
+#sudo docker system prune -f
 
-sudo docker images
+#sudo docker images
 sudo docker login -u"$USER" -p"$PASS" 
-docker push kenneth1521412/service1:latest
+
 
